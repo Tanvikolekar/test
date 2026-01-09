95ea11bd16fc6c8c9fc6c6011aa5b0b233f01a04
tanvi kolekar <kolekartanvi76@gmail.com>
2026-01-09 08:16:03 +0530

    Fix CORS configuration for production

--- single-page/backend/server.js
+++ single-page/backend/server.js
@@ -3,10 +3,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js"; 
 import connectDB from "./config/db.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://mern-frontend-7nf2.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
+
 
 // Middleware
 app.use(cors());
